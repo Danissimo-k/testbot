a5e57dc8e3ba99f6e93132364b096822b43a8587
Danil <danil0420022@gmail.com>
2021-01-29 11:03:16 +0700

    1

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import telebot
 bot = telebot.TeleBot('1634119418:AAHzc2yueiOxf-MxxzAfLp4t4A5AFBLvwNg')
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-         bot.reply_to(message, f'Халоф, я обычный пацанчик с района. Приятно познакомиться, {message.from_user.first_name}')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Халоф!')
-    else:
-        bot.send_message(message.from_user.id, 'Что ты несешь.')
-
-    bot.polling(none_stop=True)
+bot.polling()
